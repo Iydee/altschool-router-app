24a981bf031eb77b0d2656f426fe6c155beae0e3
Salami Moses <salamihydowu@yahoo.com>
2022-10-28 16:22:29 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -25,14 +25,15 @@ function Home() {
     <section className="home-container">
       <h1>Home Page</h1>
       <p>This is the Home Page</p>
-      Click below to go to the{" "}
+      Click{" "}
       <a className="Nav" href="/about">
         About Page
-      </a>
-      You can also click here to go to the{" "}
+      </a>{" "}
+      to go to the About Page. You can also click{" "}
       <a className="Nav" href="/User">
         User Page
-      </a>
+      </a>{" "}
+      to go to the User Page.
     </section>
   );
 }
@@ -42,43 +43,72 @@ function About() {
     <section className="about-container">
       <h1>About Page</h1>
       <p>This is the About Page</p>
-      Click below to go to the{" "}
+      Click{" "}
       <a className="Nav" href="/">
         Home Page
-      </a>
-      You can also click here to go to the{" "}
+      </a>{" "}
+      to go to the HomePage. You can also click{" "}
       <a className="Nav" href="/User">
         User Page
-      </a>
+      </a>{" "}
+      to go to the User Page.
     </section>
   );
 }
 
-function User() {
-  // const request = new XMLHttpRequest();
-  // request.open("GET", "https://randomuser.me/api/?page=3&results=10&seed=abc");
-  // request.send();
-  // request.onload = () => {
-  //   if (request.status === 200) {
-  //     console.log(JSON.parse(request.response));
-  //   } else {
-  //     console.log("error ${request.response}");
-  //   }
-  // };
+// class ErrorBoundary extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { hasError: false };
+//   }
 
-  // var users = request.response;
-  // console.log(users);
+//   static getDerivedStateFromError(error) {
+//     return { hasError: true };
+//   }
+
+//   componentDidCatch(error, errorInfo) {
+//     // logErrorToMyService(error, errorInfo);
+//   }
+
+//   render() {
+//     if (this.state.hasError) {
+//       return <h1>Something went wrong.</h1>;
+//     }
+
+//     return this.props.children;
+//   }
+// }
+
+function User() {
+  const [users, setUsers] = useState();
+  const [data, setData] = useState(0);
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const users = data[0];
+    console.log(data);
+  }, []);
+
+  const handleNext = (event) => {
+    event.preventDefault();
+  };
+
+  const handlePrevious = (event) => {
+    event.preventDefault();
+  };
 
   // const UsingFetch = () => {
 
   const fetchData = () => {
-    fetch("https://randomuser.me/api/?page=3&results=10&seed=abc")
+    fetch("https://randomuser.me/api/?results=10")
       .then((response) => {
         // console.log(response.json());
         return response.json();
       })
-      .then((data) => {
-        setUsers(data);
+      .then((response) => {
+        // console.log(response.results);
+        setUsers(response.results);
       });
   };
 
@@ -87,16 +117,44 @@ function User() {
   }, []);
 
   return (
-    <div>
+    <div class="card">
+      <h1>User Page</h1>
+      <p>This is the User Page</p>
+      Click
+      <a className="Nav" href="/about">
+        About Page
+      </a>{" "}
+      to go to the About Page. You can also click here to go to the
+      <a className="Nav" href="/">
+        Home Page
+      </a>{" "}
+      to go to the HomePage.
       {users.length > 0 && (
-        <ul>
+        <div class="container">
           {users.map((user) => (
-            <li key={user.id}>{user.name[0]}</li>
+            <div className="box" key={user.id.name}>
+              <b>
+                Name: {user.name.title} {user.name.first} {user.name.last}
+              </b>
+              <p>
+                {" "}
+                DOB: {user.dob.age} Gender: {user.gender}
+              </p>
+              <p>Email: {user.email}</p>
+              <p>Phone No: {user.phone}</p>
+            </div>
           ))}
-        </ul>
+        </div>
       )}
+      <button className="Nav" onClick={handlePrevious}>
+        Previous
+      </button>
+      <button className="Nav" conclick={handleNext}>
+        Next
+      </button>
     </div>
   );
+
   // }
 
   // return (
@@ -131,7 +189,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          {/* <ErrorBoundary> */}
           <Route path="/user" element={<User />} />
+          {/* </ErrorBoundary> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </section>
